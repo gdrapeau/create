10bce85197920ddb69acaac17324a9b88df34a4a
gdrapeau <github@r1d3.com>
2025-06-23 12:03:29 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 echo npx test of script.sh got $1 and $2 pwd=`pwd`
 app=$1
 repo="gdrapeau/y1"
+rm -rf $app
 mkdir $app
 cd $app
 wget -P master.zip https://github.com/$repo/archive/master.zip -O master.zip
