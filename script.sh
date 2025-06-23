03f2cfc6d3bd2379c4e46d21d2208d42850b4843
gdrapeau <github@r1d3.com>
2025-06-23 16:48:06 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,14 +8,15 @@ user=${3:-gdrapeau}
 mv $app $app-`date +%y%m%d_%H%M%S`
 rm -rf $app
 mkdir $app
-
 cd $app
+
 wget -P master.zip https://github.com/$user/$repo/archive/master.zip -O master.zip
 unzip master.zip
 mv ${repo}-main/* .
-mv ${repo)-main/.* .
+mv ${repo}-main/.* .
 rm -rf ${repo}-main
 rm master.zip
+
 chmod a+x do/*
 do/init-download $app $repo $user $4 $5 $6
 ls -a .
